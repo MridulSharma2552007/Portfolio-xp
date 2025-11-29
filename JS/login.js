51675c223398296cf030215e2bbd948e52cae500
MridulSharma2552007 <mridul25sharma52007@gmail.com>
2025-11-29 20:54:49 +0530

    Design tweeked

--- JS/login.js
+++ JS/login.js
@@ -1,4 +1,12 @@
-document.getElementById("login-btn").addEventListener("click",()=>{
-    window.location.href ="desktop.html";
-}
-);
+document.getElementById("login-btn").addEventListener("click", () => {
+
+    document.body.classList.add("fade-out");   // start fade
+
+    setTimeout(() => {
+        window.location.href = "desktop.html"; // go to next page
+    }, 500);   // same as CSS transition time (0.5s)
+});
+
+document.getElementById("restart-btn").addEventListener("click",()=>{
+    window.location.href="index.html"
+},);
